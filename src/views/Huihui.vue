49f2de60959cc8e6bb924832c76e45a8a60d1c7d
灰一样的感觉 <8417614+dmj1234@user.noreply.gitee.com>
2020-12-08 03:57:22 +0800

    封装出一个model

--- src/views/Huihui.vue
+++ src/views/Huihui.vue
@@ -1,4 +1,3 @@
-<!--suppress ALL -->
 <template>
   <Layout  class-prefix="layout">
     {{recordList}}
@@ -17,24 +16,17 @@ import Tags from '@/components/Huihui/Tags.vue';
 import Types from '@/components/Huihui/Types.vue';
 import Notes from '@/components/Huihui/Notes.vue';
 import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
 
-const recordList: Record[] =JSON.parse(window.localStorage.getItem('recordList')|| '[ ]');
-
-type Record = {
-  tags: string[] ;
-  notes: string;
-  type: string;
-  amount: number;
-  createdAt?: Date ;
-}
+const recordList = model.fetch();
 
 @Component({
   components:{Tags , Notes , Types, NumberPad}
 })
 export  default  class  Huihui extends  Vue {
   tags= ['衣', '食' ,'住', '行'];
-  recordList: Record[] = recordList;
-  record: Record = {
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags:[], notes:'', type:'-', amount:0
   };
 
@@ -45,13 +37,13 @@ export  default  class  Huihui extends  Vue {
     this.record.notes= value;
   }
   saveRecord(){
-    const record2: Record= JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem= model.clone(this.record);
     record2.createdAt= new Date();
     this.recordList.push(record2);
   }
   @ Watch('recordList')
   onRecordListChange(){
-    window.localStorage.setItem('recordList' , JSON.stringify(this.recordList));
+   model.save(this.recordList);
   }
 }
 </script>
