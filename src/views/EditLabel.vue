2880d9009b344610a47d9e557807dfeb627f06b2
灰一样的感觉 <8417614+dmj1234@user.noreply.gitee.com>
2020-12-09 18:01:11 +0800

    展示tag name

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,73 @@
+<template>
+<Layout>
+  <div class="navBar">
+    <Icon  class="leftIcon" name="left"/>
+    <span class="title">编辑标签</span>
+    <span class="rightIcon"></span>
+  </div>
+  <div class="form-wrapper">
+    <FormItem  :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+  </div>
+  <div class="button-wrapper">
+    <Button>
+      删除标签
+    </Button>
+  </div>
+  </Layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator'
+import tagListModel from '@/models/tagListModel';
+import FormItem from '@/components/Huihui/FormItem.vue';
+import Button from '@/components/Button.vue';
+@Component({
+  components:{Button, FormItem}
+})
+export default class EditLabel extends Vue{
+  tag?: {id: string; name: string} = undefined;
+created(){
+  const id = this.$route.params.id;
+  tagListModel.fetch();
+  const tags = tagListModel.data;
+  const tag = tags.filter(x => x.id === id)[0];
+  if (tag) {
+    this.tag = tag;
+  }else{
+    this.$router.replace('/404');
+  }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.navBar{
+  text-align: center;
+  padding: 12px 16px;
+  font-size: 16px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  >.title{
+
+  }
+  >.leftIcon{
+    width: 24px;
+    height: 24px;
+  }
+  >.rightIcon{
+    width: 24px;
+    height: 24px;
+  }
+}
+.form-wrapper{
+  background: white;
+  margin-top: 8px;
+}
+.button-wrapper{
+  text-align: center;
+  padding: 16px;
+  margin-top: 28px;
+}
+</style>
